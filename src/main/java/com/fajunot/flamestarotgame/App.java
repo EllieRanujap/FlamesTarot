0f0bd755f868ef7725f2313e08b4c060027955cb
EllieRanujap <wckpajunar33@gmail.com>
2026-09-14 10:48:14 +0800

    Initial Commit

--- src/main/java/com/fajunot/flamestarotgame/App.java
+++ src/main/java/com/fajunot/flamestarotgame/App.java
@@ -9,53 +9,6 @@ Date Submitted: N/A
 Summary: This the file for the final project. The app is a FLAMES game with a tarot aesthetic.
 */
 
-/*
-====================
-Notes ni Willie
-====================
-1. STAGE
-    - an object; the actual application window (with the x and everything)
-    Declaration: Stage <stage_name>
-    Import: javafx.stage.Stage;
-
-2. SCENE
-    - an object; the particular scene like in a play, display.
-    Declaration: Scene <scene_name>
-    Import: javafx.scene.Scene;
-
-3. NODE
-    - things inside the scene.
-    - ex. Text, TextField, Label, Button, ImageView, Rectangle, Circle, Pane
-
-The base scene graph:
-Stage -> Scene -> StackPane -> Label
-
-4. LABEL
-    - use it for UI. If not, use text. 
-    Import: javafx.scene.control.Label;
-
-5. APPLICATION
-    - the child of Application and must have the same name as the file.
-
-6. START
-    - the first to run.
-    Usually found:
-    - Create player, create thinngs, background, buttons, window etc.
-
-stage.setTitle("<Title>");
-    - makes the title of the window
-
-var - automatic variable identification unlike C.
-ImageView - might be used later
-Rectangle - Maybe for bars
-Button - Menu 
-
-EVENT LISTENERS:
-clickMe.setOnAction( event -> {
-    System.out.println("YOU CLICKED ME!");
-});
-*/
-
 package com.fajunot.flamestarotgame;
 
 import javafx.application.Application;
@@ -71,6 +24,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
+
 /**
  * JavaFX App
  */
@@ -79,6 +33,7 @@ public class App extends Application {
     private Stage stage;
     private Scene scene;
     private String name1, name2;
+    
     
     //Start =======================================================================
     @Override
@@ -182,7 +137,47 @@ public class App extends Application {
             
             this.name2 = name;
             txt_err.setText(this.name1 + " and " + this.name2 + "... What kind of a pair will this turn out to be?");
-            
+            flamesResult();
+        });
+    }
+    
+    public void flamesResult(){
+        //Initialization
+        int score1, score2, sum;
+        String relation;
+        score1 = getSimScore(this.name1, this.name2);
+        score2 = getSimScore(this.name2, this.name1);
+        sum = score1 + score2;
+        
+        switch (sum % "FLAMES".length()){
+            case 1: relation = "FRIENDS"; break;
+            case 2: relation = "LOVERS"; break;
+            case 3: relation = "ACQUAINTANCE"; break;
+            case 4: relation = "MARRIED"; break;
+            case 5: relation = "ENEMIES"; break;
+            default: relation = "SOULMATE"; break;
+        }
+        
+        //Elements
+        var root = new VBox();
+        var txt_score1 = new Label(this.name1 + ": " + score1);
+        var txt_score2 = new Label(this.name2 + ": " + score2);
+        var result = new Label("You are " + relation + "!");
+        var btn_return = new Button("RETURN");
+        
+        
+        //Building
+        root.getChildren().add(txt_score1);
+        root.getChildren().add(txt_score2);
+        root.getChildren().add(result);
+        root.getChildren().add(btn_return);
+        
+        //Setup
+        setupScreen(root);
+        
+        //Button Behavior
+        btn_return.setOnAction(click -> {
+            mainMenu();
         });
     }
     
@@ -200,7 +195,20 @@ public class App extends Application {
     }
     
     //CALCULATING FUNCTIONS ===================================================================================
-    
+    public int getSimScore(String n1, String n2){
+        int sim = 0;
+        char c;
+        n1 = n1.toUpperCase();
+        n2 = n2.toUpperCase();
+        
+        for (int i = 0; i < n1.length(); i++){
+            c = n1.charAt(i);
+            if ((n2.indexOf(c) != -1) && (c != ' '))
+                sim++;
+        }
+        
+        return sim;
+    }
     
     //AESTHETIC FUNCTIONS =====================================================================================
 
