bd15dd6ed986a5ddae7ab9dc37d37154d5ce2d8e
vingu <vingu@Trixter>
2026-09-14 14:39:21 +0800

    hi trix

---
+++ src/main/java/com/fajunot/flamestarotgame/AppLauncher.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fajunot.flamestarotgame;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
